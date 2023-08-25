0354084d23d304abb666ed497424bee61f5529c0
mizuochi0730 <123729606+mizuochi0730@users.noreply.github.com>
2023-08-25 18:39:38 +0900

    残り位置調整

--- Assets/Scripts/GameManeger.cs
+++ Assets/Scripts/GameManeger.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 using System;
 
 public class GameManeger : MonoBehaviour
-{ 
-   
+{
+    public Vector3 offset = new Vector3(0f, 0f,-30f);
+    public int numberOfPrefabs = 5;
+    Vector3 originalPosition = new Vector3(0f, 0f, 0f);
+    Vector3 originePositionChange = new Vector3(50, 0, 50);
+    Vector3 basePosition;
+
     List<string> suto = new List<string>
     {
         "Club",
@@ -20,7 +25,9 @@ public class GameManeger : MonoBehaviour
     };
 
     List<(string suto, string number)> cards=new List<(string suto, string number)>();
+
     // Start is called before the first frame update
+    [Obsolete]
     void Start()
     {
         // make cards
@@ -46,7 +53,7 @@ public class GameManeger : MonoBehaviour
             for (int j = 1; j <= numberOfCardsPerSuit; j++)
             {
                 // カード名を生成 (例: "Diamond01", "Diamond02", ... "Heart13")
-                string cardName = suit + j.ToString("00");
+                string cardName = suit+ j.ToString("00");
 
                 // "Black_PlayingCards_" + カード名 + "_00" の名前のGameObjectを検索
                 GameObject cardObject = GameObject.Find("Black_PlayingCards_" + cardName + "_00");
@@ -56,43 +63,78 @@ public class GameManeger : MonoBehaviour
             }
         }
         // make choosed cards List
-        List<string> pickedCard = new List<string>();
+        //List<string> pickedCard = new List<string>();
         // choose 5 card
         // for文で５回選ぶ
-        for (int i = 0; i < 5; i++)
-        {
-            int count = 0;
-            do
-            {
-                // make picked card
-                int randomCardSutoNumber = UnityEngine.Random.Range(0, 4);
-                // choose the number
-                int randomCardNumber = UnityEngine.Random.Range(0, 13);
-                Debug.Log(suto[randomCardSutoNumber] + cardNumber[randomCardNumber]);
-                // add picked card
-                pickedCard.Add(suto[randomCardSutoNumber] + cardNumber[randomCardNumber]);
-                
-                // check duplicate
-                bool duplicateFound = false;
-                for (int e = 0; e < count; e++)
-                {
-                    if (pickedCard[e] == suto[randomCardSutoNumber] + cardNumber[randomCardNumber])
-                    {
-                        duplicateFound = true;
-                        break;
-                    }
-                }
-                if (!duplicateFound)
-                {
-                    pickedCard[count] = suto[randomCardSutoNumber] + cardNumber[randomCardNumber];
-                    count++;
-                }
-            } while (count < 1);
+        //for (int i = 0; i < numberOfPrefabs; i++)
+        //{
+        //    int count = 0;
+        //    do
+        //    {
+        //        // make picked card
+        //        int randomCardSutoNumber = UnityEngine.Random.Range(0, 4);
+        //        // choose the number
+        //        int randomCardNumber = UnityEngine.Random.Range(0, 13);
+        //        Debug.Log(suto[randomCardSutoNumber] + cardNumber[randomCardNumber]);
+        //        // add picked card
+        //        pickedCard.Add(suto[randomCardSutoNumber] + cardNumber[randomCardNumber]);
 
+        //        // check duplicate
+        //        bool duplicateFound = false;
+        //        for (int e = 0; e < count; e++)
+        //        {
+        //            //if (pickedCard[e] == suto[randomCardSutoNumber] + cardNumber[randomCardNumber])
+        //            if(string.Equals(pickedCard[e], suto[randomCardSutoNumber] + cardNumber[randomCardNumber]))
+        //            {
+        //                duplicateFound = true;
+        //                pickedCard.RemoveAt(pickedCard.Count - 1);
+        //                break;
+        //            }
+        //        }
+        //        if (!duplicateFound)
+        //        {
+        //            pickedCard[count] = suto[randomCardSutoNumber] + cardNumber[randomCardNumber];
+        //            count++;
+        //        }
+        //    } while (count < 1);
+        //}
+        HashSet<string> pickedCard = new HashSet<string>();
+
+        int count = 0;
+        while (count < numberOfPrefabs)
+        {
+            int randomCardSutoNumber = UnityEngine.Random.Range(0, 4);
+            int randomCardNumber = UnityEngine.Random.Range(0, 13);
+            string card = suto[randomCardSutoNumber] + cardNumber[randomCardNumber];
+
+            if (!pickedCard.Contains(card))
+            {
+                pickedCard.Add(card);
+                count++;
+                Debug.Log(card);
+            }
         }
+
+
+        basePosition = originalPosition + originePositionChange;
+        Debug.Log(pickedCard.Count);
+        
+        //perfectPositionを操作が終わった後に初期化する
+
         //選んだカードのリストをgameObjectのリストに変換
         //gameObjectのリストのカードを並べる
+        //Instantiate(prefabToSpawn, transform.position, transform.rotation);
+        //GameObject card01 = cardNameToObject["Club01"];
+        //Vector3 newScale = new Vector3(2.0f, 2.0f, 2.0f);
+        //Vector3 newScale = new Vector3(2.0f, 2.0f, 2.0f);
 
+        foreach (string inDictionary in pickedCard)
+        {
+            Vector3 position = basePosition;
+            basePosition = basePosition + offset;
+            Instantiate(cardNameToObject[inDictionary], position, transform.rotation);
+        }
+       
     }
 
     // Update is called once per frame
