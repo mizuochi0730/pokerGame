802e5cffe6aa258eeecee8f1496c0e7353e0d27b
mizuochi0730 <123729606+mizuochi0730@users.noreply.github.com>
2023-11-10 20:12:37 +0900

    11月10日

--- Assets/Scripts/GameManeger.cs
+++ Assets/Scripts/GameManeger.cs
@@ -10,16 +10,14 @@ public class GameManeger : MonoBehaviour
     //カードの配列
     public GameObject[] blackPlayingCards;
     //ターンの処理の定義
-    enum Turn
-    {
-        player,Enemy1,Enemy2,Enemy3
-    }
     List<string> playerCards = new List<string>();
     List<string> EnemyCards1 = new List<string>();
     List<string> EnemyCards2 = new List<string>();
     List<string> EnemyCards3 = new List<string>();
+
     //シャッフルの定義
-    void Shuffle<T>(T[] blackPlayingCards)
+
+    void Shuffle<S>(S[] blackPlayingCards)
     {
         Random ram = new Random();
 
@@ -27,7 +25,7 @@ public class GameManeger : MonoBehaviour
         for (int i = blackPlayingCards.Length - 1; i > 0; i--)
         {
             int j = ram.Next(i + 1);
-            T temp = blackPlayingCards[i];
+            S temp = blackPlayingCards[i];
             blackPlayingCards[i] = blackPlayingCards[j];
             blackPlayingCards[j] = temp;
         }
@@ -36,176 +34,80 @@ public class GameManeger : MonoBehaviour
 
     void Start()
     {
-        //カードの配列にカードを入れる
-        //for (int i = 1; i <= 13; i++)
-        //{
-        //    blackPlayingCards[i - 1] = (GameObject)Resources.Load("Black_PlayingCards_Club" + i.ToString("00"));
-        //}
-        //for (int i = 1; i <= 13; i++)
-        //{
-        //    blackPlayingCards[i + 12] = (GameObject)Resources.Load("Black_PlayingCards_Diamond" + i.ToString("00"));
-        //}
-        //for (int i = 1; i <= 13; i++)
-        //{
-        //    blackPlayingCards[i + 25] = (GameObject)Resources.Load("Black_PlayingCards_Heart" + i.ToString("00"));
-        //}
-        //for (int i = 1; i <= 13; i++)
-        //{
-        //    blackPlayingCards[i + 38] = (GameObject)Resources.Load("Black_PlayingCards_Spade" + i.ToString("00"));
-        //}
-        //ここで配列をシャッフルする
+         //ここで配列をシャッフルする
         Shuffle(blackPlayingCards);
         int ofsetPlayer = 22;
         int ofsetEnemy = 18;
         //ここでシャッフルした配列の最初から５枚ずつカードを生成
+
+
+        // kimoi
         for (int y = 0; y < 20; y++)
         {
             if (0 <= y && y <= 4)
             {
                 Instantiate(blackPlayingCards[y], new Vector3(-50 + y * ofsetPlayer, 14, 60), Quaternion.Euler(50, 0, 0));
-                playerCards.Add
+                playerCards.Add(blackPlayingCards[y].name);
             }
             if (5 <= y && y <= 9)
             {
                 Instantiate(blackPlayingCards[y], new Vector3(58, 14, 40 + y * ofsetEnemy * -1), Quaternion.Euler(90, 90, -20));
-                EnemyCards1.Add
+                EnemyCards1.Add(blackPlayingCards[y].name);
             }
             if (10 <= y && y <= 14)
             {
                 Instantiate(blackPlayingCards[y], new Vector3(36 + y * ofsetEnemy * -1, 14, -40), Quaternion.Euler(-100, 0, 0));
-                EnemyCards2.Add
+                EnemyCards2.Add(blackPlayingCards[y].name);
             }
             if (15 <= y && y <= 19)
             {
                 Instantiate(blackPlayingCards[y], new Vector3(-57, 14, -40 + y * ofsetEnemy), Quaternion.Euler(90, -90, 20));
-                EnemyCards3.Add
+                EnemyCards3.Add(blackPlayingCards[y].name);
             }
+       
         }
+        // kimoi owari
 
-
-    }
-    void Update()
-    {
-        //手札の処理
-        if (/*自分のターン*/)
+        for (int i = 0; i <= 4; i++)
         {
-            if(/*press 選ぶボタン*/)
-            {
-                //カードを選んだ状態にする 
-            }
-            if (/*press 捨てるボタン*/)
-            {
-                //選ばれたカードを捨てる
-            }
-            ++
+            Debug.Log(playerCards[i]);
         }
-        if(/*Enemy1 || Enemy2 || Enemy3 のターン*/)
+        //手札の処理
+        int t = 0;
+        if (t == 0)
+        {
+            //if (/*press 選ぶボタン*/)
+            //{
+            //    //カードを選んだ状態にする 
+            //}
+            //if (/*press 捨てるボタン*/)
+            //{
+            //    //選ばれたカードを捨てる
+            //}
+            t++;
+        }
+        if (t == 1)
         {
             //期待値が一番高くなるようにカードを選ぶ
             //カードを捨てる
+            t++;
+        }
+        if(t == 2)
+        {
+            t++;
+        }
+        if(t == 3)
+        {
+            t++;
         }
 
 
     }
-    //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+    void Update()
+    {
+        
 
 
-    //遺産
-
-    // dictionaryを作成
-    //Dictionary<string, GameObject> cardNameToObject = new Dictionary<string, GameObject>();
-
-    //// スートの配列
-    //string[] suits = { "Diamond", "Club", "Spade", "Heart" };
-    //int numberOfCardsPerSuit = 13;
-
-    //// 各スートに対して1から13までのカードを生成して辞書に追加
-    //foreach (string suit in suits)
-    //{
-    //    // 1から13までのカードを作成
-    //    for (int j = 1; j <= numberOfCardsPerSuit; j++)
-    //    {
-    //        // カード名を生成 (例: "Diamond01", "Diamond02", ... "Heart13")
-    //        string cardName = suit+ j.ToString("00");
-
-    //        // "Black_PlayingCards_" + カード名 + "_00" の名前のGameObjectを検索
-    //        GameObject cardObject = GameObject.Find("Black_PlayingCards_" + cardName + "_00");
-
-    //        // 辞書にカード名をキーとして、対応するGameObjectを追加
-    //        cardNameToObject.Add(cardName, cardObject);
-    //    }
-    //}
-    // make choosed cards List
-    //List<string> pickedCard = new List<string>();
-    // choose 5 card
-    // for文で５回選ぶ
-    //for (int i = 0; i < numberOfPrefabs; i++)
-    //{
-    //    int count = 0;
-    //    do
-    //    {
-    //        // make picked card
-    //        int randomCardSutoNumber = UnityEngine.Random.Range(0, 4);
-    //        // choose the number
-    //        int randomCardNumber = UnityEngine.Random.Range(0, 13);
-    //        Debug.Log(suto[randomCardSutoNumber] + cardNumber[randomCardNumber]);
-    //        // add picked card
-    //        pickedCard.Add(suto[randomCardSutoNumber] + cardNumber[randomCardNumber]);
-
-    //        // check duplicate
-    //        bool duplicateFound = false;
-    //        for (int e = 0; e < count; e++)
-    //        {
-    //            //if (pickedCard[e] == suto[randomCardSutoNumber] + cardNumber[randomCardNumber])
-    //            if(string.Equals(pickedCard[e], suto[randomCardSutoNumber] + cardNumber[randomCardNumber]))
-    //            {
-    //                duplicateFound = true;
-    //                pickedCard.RemoveAt(pickedCard.Count - 1);
-    //                break;
-    //            }
-    //        }
-    //        if (!duplicateFound)
-    //        {
-    //            pickedCard[count] = suto[randomCardSutoNumber] + cardNumber[randomCardNumber];
-    //            count++;
-    //        }
-    //    } while (count < 1);
-    //}
-    //HashSet<string> pickedCard = new HashSet<string>();
-
-    //int count = 0;
-    //while (count < numberOfPrefabs)
-    //{
-    //    int randomCardSutoNumber = UnityEngine.Random.Range(0, 4);
-    //    int randomCardNumber = UnityEngine.Random.Range(0, 13);
-    //    string card = suto[randomCardSutoNumber] + cardNumber[randomCardNumber];
-
-    //    if (!pickedCard.Contains(card))
-    //    {
-    //        pickedCard.Add(card);
-    //        count++;
-    //        Debug.Log(card);
-    //    }
-    //}
-
-
-    //basePosition = originalPosition + originePositionChange;
-    //Debug.Log(pickedCard.Count);
-
-    //perfectPositionを操作が終わった後に初期化する
-
-    //選んだカードのリストをgameObjectのリストに変換
-    //gameObjectのリストのカードを並べる
-    //Instantiate(prefabToSpawn, transform.position, transform.rotation);
-    //GameObject card01 = cardNameToObject["Club01"];
-    //Vector3 newScale = new Vector3(2.0f, 2.0f, 2.0f);
-    //Vector3 newScale = new Vector3(2.0f, 2.0f, 2.0f);
-
-    //foreach (string inDictionary in pickedCard)
-    //{
-    //    Vector3 position = basePosition;
-    //    basePosition = basePosition + offset;
-    //    Instantiate(cardNameToObject[inDictionary], position, transform.rotation);
-    //}
-
+    }
 }
